15b71273923049ab6724ea80ce91cc886f0b5545
williammilkyan <williammilkyan@hotmail.com>
2023-08-19 15:05:42 -0700

    2nd edit

--- server.js
+++ server.js
@@ -1,88 +1,115 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const http = require("http").createServer(app);
 
-// [other modules]
 const compressImages = require("compress-images");
 const formidable = require("express-formidable");
-const mysql =require('mysql');
-app.use(formidable());
+const mysql = require('mysql');
 
+app.use(express.json());
+app.use(formidable());
+app.use(cors());
 const fileSystem = require("fs");
 app.set("view engine", "ejs");
-app.use(express.static("uploads"));
+app.use(express.static("public"));
 
-const port = process.env.PORT || 3000;
+
+const port = 3000;
 
 let filePath = "";
 let compressedFilePath = "";
 
 
-const db = mysql.createConnection({
+let config = [
+    {
+        path: '/fetchFunctionEnum',
+        picExt: ['png', 'jpg', 'jpeg', 'gif', 'bmp', 'svg'],
+        quality: 1.0, //0.1-1.0, default is 1.0
+        backUpOrg: false, //default is false
+        backUpOrgPath: 'backUpOrg', //default is backUpOrg
+        createThumbnail: false, //default is true
+        thumbnailPath: 'thumbnail', //default is thumbnail
+
+    }]
+
+
+const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '1234',
     database: 'nodemysqlfs'
   });
 
-http.listen(port, () => {
-    console.log("Server started running at port: " + port);
 
     // [post route goes here]
     // upload image
-    app.post("/compressImage", (req, res) =>{
-        const image = req.files.image;
+
+
+app.post("/compressImage", (req, res) =>{
+    const image = req.files.image;
         
-        if(image.size > 0) {
+    if(image.size > 0) {
 
-            if(image.type == "image/png" || image.type == "image/jpeg") {
-                fileSystem.readFile(image.path, function (error, data) {
+        if(image.type == "image/png" || image.type == "image/jpeg" || image.type == "image/JPEG" || image.type == "image/PNG") {
+            fileSystem.readFile(image.path, function (error, data) {
+            if (error) throw error;
+            config[0].path = image.path;
+            config[0].picExt = image.type;
+                filePath = "public/uploads/" + (new Date().getTime()) + "-" + image.name;
+                let newName = image.name.replace(".", "");
+
+                compressedFilePath = "public/uploads/" + newName;
+                const compression = 60;
+                fileSystem.writeFile(filePath, data, async function (error){
                     if (error) throw error;
-
-                     filePath = "uploads/" + (new Date().getTime()) + "-" + image.name;
-                     compressedFilePath = "uploads/" + image.name;
-                    const compression = 60;
-
-                    fileSystem.writeFile(filePath, data, async function (error){
-                        if (error) throw error;
-
-                        handleimg(filePath, compressedFilePath, compression, image);
-
-                            
-                            
-
-                           
-                            res.render("success.ejs", {
-                                rpath: filePath
-                            })
-                        })
-
-
-                    
-
-                        // remove temp file in document/user
-                        /*fileSystem.unlink(image.path, function (error) {
-                            if (error) throw error;
-                        }) */
-
-                        
+                        handleimg(filePath, compressedFilePath, compression, image);                           
                     })
-                } else {
-                    res.send("Please select an image");
-                }
+                     config[0].backUpOrgPath = filePath.replace("public", "");
+                     config[0].backUpOrg = "true"
+                     config[0].thumbnailPath = "/uploads/" + newName + filePath.replace("public/uploads/", "");
+                   
+                    
+                    res.render("process.ejs", {
+                            oPath: config[0].backUpOrgPath,
+                            
+                    })
+                        // remove temp file in document/user
+                        fileSystem.unlink(image.path, function (error) {
+                            if (error) throw error;
+                        }) 
+            })
             } else {
-                res.send("Please select an image");
-            }
-        })
-    // [get route goes here]
-    app.get("/", (req, res) => {
+                res.render("index.ejs");
+                    }
+    } else {
         res.render("index.ejs");
-    })
+            }
+            
+})
+
+
+
+    // [get route goes here]
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.get("/success", (req, res) => {
+    res.render("success.ejs", {
+        oPath: config[0].backUpOrgPath,
+        cPath: config[0].thumbnailPath
+    });
+    console.log(config[0]);
+    
+})
+
+app.listen(port, () => {
+    console.log("Server started running at port: " + port);
 })
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
   // compress the image
 async function pressimg(filePath, compressedFilePath, compression) {
@@ -108,28 +135,32 @@ async function pressimg(filePath, compressedFilePath, compression) {
             
         })
         await delay(3000);
+        
         console.log(result + ' line 110');
         return result;
 }
 // upload to mysql 
-function uploaddb(image, result) {
-    db.connect((err) => {
+function uploaddb() {
+    db.getConnection((err, connection) => {
         if (err) throw err;
         console.log('Connected to MySQL database');
-        let sql = `UPDATE images SET path = '${image.path}', picExt = '${result.algorithm}', quality = ${result.percent}, backUpOrg = 1, backUpOrgPath = '${result.input}', createThumbnail = true, thumbnailPath = '${result.path_out_new}' WHERE id = 1`;
-        db.query(sql, function (err, result) {
+        let sql = `INSERT INTO images (Original_image, Compressed_image) VALUES (?, ?)`;
+        connection.query(sql, [ config[0].backUpOrgPath, config[0].thumbnailPath],function (err, result) {
             if (err) throw err;
-            console.log(result.affectedRows + " record(s) updated");
+            console.log(result.affectedRows + " record(s) insert");
+            connection.release();
           });
       });
 }
-
+//combine  pressimg and uploaddb
 async function handleimg(filePath, compressedFilePath, compression, image) {
     try {
-
     let resultf = await pressimg(filePath, compressedFilePath, compression);
     console.log(resultf + 'line 130');
-    uploaddb(image, resultf);
+    
+    uploaddb();
+    config[0].quality = resultf.percent;
+    config[0].createThumbnail = 'true';
     } catch (err) {
         console.log(err);
     }
